2f48941ff1dbf79350fa4d73a22ea66709b97e97
Bianca Peixoto <biancapeixotolima06@gmail.com>
2026-03-01 15:01:32 -0300

    carro pronto

---
+++ ClasseObjeto/src/exe0/Carro.java
@@ -0,0 +1,73 @@
+package exe0;
+public class Carro {
+    //crianda variáveis com visibilidade pública
+    //qualquer outra calsse pode acessar e alterar essas variáveis
+    public String marca, modelo, cor, placa;
+    public float potencia, velocidade;
+    public int ano;
+    public boolean motor;
+
+
+     //metodo construtor
+    public Carro(String marca, String modelo, float potencia, int ano, String placa, String cor){
+        this.marca =  marca; this.modelo = modelo;
+        this.potencia = potencia; this.ano = ano;
+        this.placa = placa; this.cor = cor;
+        this.motor = false; this.velocidade = 0;
+    }
+        //ligar carro
+        public void ligar(){
+        if (!this.motor){
+            this.motor = true;
+            System.out.println("Motor foi ligado");
+        }
+    }
+
+
+    //desliga carro
+    public void desligar(){
+        if (this.motor ){
+            this.motor = false;
+            System.out.println("Motor foi desligado");
+            this.velocidade = 0;
+        }
+    }
+
+
+        //acelerar o carro de x unidades
+    public void acelerar (float x){
+        if (this.motor){
+            this.velocidade = this.velocidade + x;
+            System.out.println("Carro acelerado, valor atual " + this.velocidade);
+        }
+    }
+    //frear
+   public void frear (float x){
+        if (this.motor && this.velocidade - x >= 0){
+            this.velocidade -=x;
+            System.out.println("Carro freado, valor atual " +this.velocidade);
+        }
+        else System.out.println("Não foi possivel frear.");
+   }
+
+
+
+
+    //converte o objeto em String
+    public String toString(){
+
+        // this representa o objeto que chama o metodo
+        return  "\nMarca: " +this.marca +
+                "\nModelo: "+this.modelo +
+                "\nPlaca: "+this.placa +
+                "\nPotencia" +this.potencia+
+                "\nAno: "+this.ano +
+                "\nMotor: " + (this.motor ? "Ligado" : "Desligado" )+
+                "\nVelocidade: "+ this.velocidade;// operador ternario
+
+
+
+
+
+    }
+}
---
+++ ClasseObjeto/src/exe0/TestaCarro.java
@@ -0,0 +1,26 @@
+package exe0;
+public class TestaCarro {
+    public static void main(String args []){
+    //instância obj1
+        Carro obj1 = new Carro("Fiat", "Palio", 1.6f, 2020, "A", "Cor");
+        System.out.println(obj1.toString());
+
+        Carro obj2 = new Carro("GM", "Onix", 1.3f, 2021, "B", "Preto");
+        System.out.println(obj2.toString());
+        obj2.ligar();
+        obj2.acelerar (80);
+        System.out.println(obj2.toString());
+
+        Carro obj3 = new Carro("Volks", "Gol", 1.0f, 2022, "C", "Branco");
+        System.out.println(obj3.toString());
+        obj3.ligar();
+        obj3.acelerar(100);
+        System.out.println(obj3.toString());
+
+        obj2.desligar();
+        obj3.desligar();
+    }
+}
+
+
+
